2cb7afe9e8f5fc06f5af4a79bfd64af9c74841c4
root <lamlamhlana999@gmail.comgit config --global user.name Lamlagit config user.email lamlamhlana@gmail.com>
2023-02-28 13:53:28 -0800

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,21 @@
-#include "main.h"
-/**
- * swap_int - swaps the values of two integers
- * @a: integer 1
- * @b: integer 2
- *
- **/
-void swap_int(int *a, int *b)
-{
-	int c = *a;
+#include <stdio.h>
 
-	*a = *b;
-	*b = c;
+void swap_int(int *, int *);
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	a = 98;
+	b = 42;
+	printf("%d, %d\n", a, b);
+	swap_int(&a, &b);
+	printf("%d, %d\n", a, b);
+	return (0);
 }
