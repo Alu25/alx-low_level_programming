11874c181137ce601f49b052f0a07726f33c6b68
root <lamlamhlana999@gmail.comgit config --global user.name Lamlagit config user.email lamlamhlana@gmail.com>
2023-02-28 13:40:09 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,11 @@
-#include "main.h"
-#include <stdio.h>
-
+#include "holberton.h"
 /**
- * main - check the code 
+ * reset_to_98 - takes a pointer to an int as parameter
+ * and updates the value it points to 98
+ * @n: parameter to take the value.
  *
- * Return: Always 0.
  */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
-	*n - 98;
-}	
+	*n = 98;
+}
